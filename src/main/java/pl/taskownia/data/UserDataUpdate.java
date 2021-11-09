9f390ba7afd96badef1b6905a31f42803d00e6ca
Kamil Korbik <korbik.gm@gmial.com>
2021-11-09 11:49:10 +0100

    Poprawki

--- src/main/java/pl/taskownia/data/UserDataUpdate.java
+++ src/main/java/pl/taskownia/data/UserDataUpdate.java
@@ -1,7 +1,9 @@
 package pl.taskownia.data;
 
+import lombok.Data;
 import pl.taskownia.model.User;
 
+@Data
 public class UserDataUpdate {
     private String email;
     private User.MakerStatus makerStatus;
@@ -13,85 +15,4 @@ public class UserDataUpdate {
     private String state;
     private String country;
     private String zipCode;
-
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public User.MakerStatus getMakerStatus() {
-        return makerStatus;
-    }
-
-    public void setMakerStatus(User.MakerStatus makerStatus) {
-        this.makerStatus = makerStatus;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
 }
