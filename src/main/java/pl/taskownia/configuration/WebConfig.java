8ab8fedfbe514af22da4839fe35cb6357f189e02
matrix14 <modeft1999@gmail.com>
2022-01-12 14:50:31 +0100

    User images resource handler repair

--- src/main/java/pl/taskownia/configuration/WebConfig.java
+++ src/main/java/pl/taskownia/configuration/WebConfig.java
@@ -1,15 +1,43 @@
 package pl.taskownia.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @EnableWebMvc
 @Component
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedHeaders("*");
+    }
+
+    private static String fileUploadsPath;
+
+    @Value("${app.file-uploads-path}")
+    public void setFileUploadsPath(String fileUploadTemp) {
+        WebConfig.fileUploadsPath = fileUploadTemp;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        exposeDirectory(fileUploadsPath, registry);
+    }
+
+    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get(dirName);
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+
+        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+
+        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:" + uploadPath + "/");
     }
 }
